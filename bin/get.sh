2b5fc44e6f94ed7a7eab84509d69611b57f9fc6f
n0001150 <Jotham.Hoag@LibertyMutual.com>
2019-08-24 11:46:09 -0400

    Refactor

--- bin/get.sh
+++ bin/get.sh
@@ -1,9 +1,9 @@
 track="$1"
 exercise="$2"
-file="$3"
 
 exercism_home="$HOME/exercism"
 webstorm_home="$HOME/WebstormProjects"
+
 exercism_file="$exercism_home/${track}/${exercise}/file"
-cp "$webstorm_home/${exercise}/$file" "$exercism_file"
-exercism submit "$exercism_file"
+
+cp "${exercism_home}/*" "${webstorm_home}/${track}_${exercise}/."
